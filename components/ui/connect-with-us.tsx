294c4937b2ca1ac4e2df195b64ba71610360f4a3
Faris Thibani <105483158+Faris-Thibani@users.noreply.github.com>
2026-02-13 12:48:26 +0000

    Import fix

--- components/ui/connect-with-us.tsx
+++ components/ui/connect-with-us.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import React from 'react';
-
 const SocialConnect = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 to-black flex flex-col items-center justify-center p-4 font-sans w-full">
